3240fc3c922d90759b4f3e927b8492cb6e9bff22
Andre Velasquez <andrethefirst21@gmail.com>
2021-08-14 13:52:44 -0700

    add api requests and error

---
+++ src/js/service.js
@@ -0,0 +1,24 @@
+export default class CurrencyService {
+  static currencyConvert(errors) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = '';
+
+      if(errors) {
+        url = 'https://v6.exchangerate-api.com/v6/12345abcde/latest/USD';
+      } else {
+        url = 'https://v6.exchangerate-api.com/v6/2911b5b91c212768e88641a5/latest/USD';
+      }
+
+      request.onload = function() {
+        if (this.money === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open('GET',url, true);
+      request.send();
+    });
+  }
+}
